eabcd9af280917bb6ce71f06877d965e9ea5f3f4
RafaelGulman <Gulman04@outlook.com>
2022-10-21 18:41:45 +0500

    Задана структура программе

---
+++ MyTextEditorProject/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MyTextEditorProject.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextEditorProject.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Properties
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
